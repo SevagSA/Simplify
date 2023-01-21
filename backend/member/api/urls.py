ea6ba46e8624ea0692a76f7e1f0a8ddc3ff0bd66
SevagSA <>
2023-01-21 15:06:41 -0500

    is first login API implementation

--- backend/member/api/urls.py
+++ backend/member/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import login_member
+from .views import login_member, is_first_login
 
 urlpatterns = [
-    path('login/', login_member)
+    path('login/', login_member),
+    path('is_first_login/', is_first_login),
 ]
