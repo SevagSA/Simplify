2c5f8bbffd8d320bf4df2f3449711b05d60b1e32
Ali Lezzeik <56936736+aliL123@users.noreply.github.com>
2023-01-21 18:19:20 -0500

    Fixed issues with spending

--- backend/transactions/api/urls.py
+++ backend/transactions/api/urls.py
@@ -3,14 +3,15 @@ from .views import (
     ExpensesList, ExpensesDetail, CardList, CardDetail,
     get_all_spendings_for_card, get_recent_spendings,
     get_expenses_for_month, get_highest_recurring_expense,
-    sum_of_all_cards_for_member)
+    sum_of_all_cards_for_member, get_all_income_for_card)
 
 urlpatterns = [
     path('expenses/', ExpensesList.as_view()),
     path('expenses/<int:pk>', ExpensesDetail.as_view()),
-    path('expenses/spendings/', get_all_spendings_for_card),
-    path('expenses/spendings/recent',get_recent_spendings),
-    path('expenses/spendings/highest', get_highest_recurring_expense),
+    path('expenses/income/<int:card>', get_all_income_for_card ),
+    path('expenses/spendings/<int:card>', get_all_spendings_for_card),
+    path('expenses/spendings/recent/<int:card>', get_recent_spendings),
+    path('expenses/spendings/highest/<int:card>', get_highest_recurring_expense),
     path('cards/', CardList.as_view()),
     path('cards/<int:pk>', CardDetail.as_view()),
     path('cards/<int:card>/<int:month>/<int:year>/<is_income>/', get_expenses_for_month),
