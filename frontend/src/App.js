6ae9d2fff3041b70cd680f09c59cb6c379215620
kkliro <konstantin-klir@outlook.com>
2023-01-21 14:10:15 -0500

    Skeleton of MonthlyPayments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Sidebar from './components/Sidebar/SidebarComponent';
 import DashboardCard from './components/DashboardCard/DashboardCard';
 import ExpensesCard from './components/ExpensesCard/ExpensesCard';
+import MonthlyPayments from './components/MonthlyPayments/ExpensesCard/MonthlyPayments';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Sidebar />
       <DashboardCard />
       <ExpensesCard />
+      <MonthlyPayments />
     </div>
   );
 }
