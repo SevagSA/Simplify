2b213c96a25c47de2509410a21541472c7d8b263
kkliro <konstantin-klir@outlook.com>
2023-01-21 16:56:00 -0500

    Added routing to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,13 @@
 import './App.css';
+
+//Routers
+// import { ReactDOM } from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+// Sidebar
 import Sidebar from './components/Sidebar/SidebarComponent';
+
+// Pages
 import Dashboard from './components/Dashboard/Dashboard';
 
 function App(){
@@ -9,7 +17,18 @@ function App(){
         <Sidebar />
       </div>
       <div>
-        <Dashboard />
+        {/* <Dashboard /> */}
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="dashboard/" element={<Dashboard />}>
+              {/* <Route index element={<Home />} />
+              <Route path="blogs" element={<Blogs />} />np
+              <Route path="contact" element={<Contact />} />
+              <Route path="*" element={<NoPage />} /> */}
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </div>
   );
