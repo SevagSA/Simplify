a76d709e54bac86c026e27c59a7127097cf09bcf
kkliro <konstantin-klir@outlook.com>
2023-01-21 15:05:23 -0500

    Skeleton of LatestSpendings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import Sidebar from './components/Sidebar/SidebarComponent';
 import DashboardCard from './components/DashboardCard/DashboardCard';
 import ExpensesCard from './components/ExpensesCard/ExpensesCard';
-import MonthlyPayments from './components/MonthlyPayments/ExpensesCard/MonthlyPayments';
+import MonthlyPayments from './components/MonthlyPayments/MonthlyPayments';
+import {LatestSpendings} from './components/LatestSpendings/LatestSpendings'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <DashboardCard />
       <ExpensesCard />
       <MonthlyPayments />
+      <LatestSpendings />
     </div>
   );
 }
