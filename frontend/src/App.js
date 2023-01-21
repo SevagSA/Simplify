83064c1f018ce8ac52c8e0539bc739bc8f68636f
kkliro <konstantin-klir@outlook.com>
2023-01-21 15:53:53 -0500

    Skeleton of PersonalGoal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,12 @@ import MonthlyPayments from './components/MonthlyPayments/MonthlyPayments';
 import {LatestSpendings} from './components/LatestSpendings/LatestSpendings'
 import DataGraph from './components/DataGraph/DataGraph';
 import { ScheduledPayments } from './components/ScheduledPayments/ScheduledPayments';
+import PersonalGoal from './components/PersonalGoal/PersonalGoal';
 
 function App(){
   return (
     <div className="App">
-      <ScheduledPayments/>
+      <PersonalGoal/>
     </div>
   );
 }
