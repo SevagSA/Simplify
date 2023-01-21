74abf332fe3e2bc0e39af5df3bd40872bf000252
kkliro <konstantin-klir@outlook.com>
2023-01-21 14:04:50 -0500

    Skeleton of ExpensesCard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import Sidebar from './components/Sidebar/SidebarComponent'
+import Sidebar from './components/Sidebar/SidebarComponent';
+import DashboardCard from './components/DashboardCard/DashboardCard';
+import ExpensesCard from './components/ExpensesCard/ExpensesCard';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        
-      </header> */}
       <Sidebar />
+      <DashboardCard />
+      <ExpensesCard />
     </div>
   );
 }
