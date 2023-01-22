661f1752b3e6a6e8200adb1236d50d02495b0532
Ali Lezzeik <56936736+aliL123@users.noreply.github.com>
2023-01-21 23:49:12 -0500

    Added default to Member

--- backend/member/models.py
+++ backend/member/models.py
@@ -39,7 +39,7 @@ class Member(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=30, default='')
     monthly_budget = models.DecimalField(max_digits=8, decimal_places=2, default=0)
-    personal_goal = models.DecimalField(max_digits=8,decimal_places=2, default=0)
+    personal_goal = models.DecimalField(max_digits=8,decimal_places=2, default=50000)
     is_first_login = models.BooleanField(default=True)
 
     is_staff = models.BooleanField(default=False)
