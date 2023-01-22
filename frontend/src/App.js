450164bc8eedc6d2c1021fc6cb6d848f074a8f87
CarmeloS <carm.scrib@gmail.com>
2023-01-21 21:35:50 -0500

    Fixed issue with display overflow and banner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,27 +15,29 @@ import CardForm from './components/CardForm/CardForm';
 
 function App(){
   return (
-    <div className="App">
-      <div className='small-width123'>
-        <Sidebar />
-      </div>
-      <div className='big-width123'>
-        <BrowserRouter>
-          <Routes>
-              <Route path="/" element={null}></Route>
-              <Route path="/dashboard" element={<Dashboard />}></Route>
-              <Route path="/transactions" element={<Transactions />}></Route>
-              <Route path="/cards" element={<UserCards />}></Route>
-              <Route path="/addcard" element={<CardForm />}></Route>
+    <div className="App background-color-class">
+      <div className='top-banner full-width1234'></div>
+      <div className='menu-items123 full-width123'>
+        <div className='small-width123'>
+          <Sidebar />
+        </div>
+        <div className='big-width123'>
+          <BrowserRouter>
+            <Routes>
+                <Route path="/" element={null}></Route>
+                <Route path="/dashboard" element={<Dashboard />}></Route>
+                <Route path="/transactions" element={<Transactions />}></Route>
+                <Route path="/cards" element={<UserCards />}></Route>
+                <Route path="/addcard" element={<CardForm />}></Route>
+                
+                {/* <Route index element={<Home />} />
+                <Route path="blogs" element={<Blogs />} />np
+                <Route path="contact" element={<Contact />} />
+                <Route path="*" element={<NoPage />} /> */}
               
-              {/* <Route index element={<Home />} />
-              <Route path="blogs" element={<Blogs />} />np
-              <Route path="contact" element={<Contact />} />
-              <Route path="*" element={<NoPage />} /> */}
-            
-          </Routes>
-        </BrowserRouter>
-
+            </Routes>
+          </BrowserRouter>
+        </div>
       </div>
     </div>
   );
