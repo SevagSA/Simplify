6e5531c4fb8af3dd6f759fc7b33428e703c115a0
Carmelo Scribano <carm.scrib@gmail.com>
2023-03-06 18:46:45 -0500

    Updates on side menu

--- frontend/src/components/Sidebar/SidebarComponent.js
+++ frontend/src/components/Sidebar/SidebarComponent.js
@@ -7,20 +7,19 @@ import { BrowserRouter, Link } from 'react-router-dom';
 export default class Sidebar extends Component{
   render(){
     return (
-      <div className="box-shadow border-radius side-menu">
-        <div className="centered-container height-10 pt-5">
-          <h1>$implify</h1>
-        </div>
+      <div className="box-shadow border-radius centered-container side-menu">
+        <h1>$implify</h1>
 
         <div style={{height:'10vh'}}></div>
 
-        <div className="user-element">
-          <div className="centered-container">
-            <img className="user-picture" src={userPicture}/>
-            <h2>Welcome back,</h2>
-            <h6>John Doe</h6>
-          </div>
-        </div>
+        <img className="user-picture mb-2" src={userPicture}/>
+        <h2>Welcome back,</h2>
+        <h6>John Doe</h6>
+
+        <div style={{height:'10vh'}}></div>
+
+        <h1>$9,000.00</h1>
+        <h2>Monthly budget</h2>
       </div>
     );
   }
