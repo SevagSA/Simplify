58ff4da68a69e87cf69b0669f9db8816c9ad1319
Carmelo Scribano <carm.scrib@gmail.com>
2023-03-09 11:41:34 -0500

    finished add transaction page

--- frontend/src/components/TransactionsForm/TransactionsForm.js
+++ frontend/src/components/TransactionsForm/TransactionsForm.js
@@ -37,7 +37,56 @@ function CreateTransaction(e){
 function TransactionsForm(card){
     return (
         <div>
-            hello there
+            <div className="page-title-container">
+                <h1 className="page-title text-black">Add Transaction</h1>
+            </div>
+            <div className="mr-8 ml-8 mb-3 mt-10">
+                <div className="flex-center width-full">
+                    <div className="height-20" style={{width:'25vw'}}>
+                        <Form onSubmit={CreateTransaction} className="form">
+                            <FormGroup>
+                                <Label for="sourceVal">Transaction source</Label>
+                                <Input id="sourceVal" type="text" placeholder='Source' required/>
+                                <Input name="cardId" value={card.id} hidden/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="category">Category</Label>
+                                <Input type="select" name="category" id="category">
+                                    <option>Food</option>
+                                    <option>Housing</option>
+                                    <option>Entertainement</option>
+                                    <option>Miscellaneous</option>
+                                </Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="tranFreq">Frequency</Label>
+                                <Input type="select" name="tranFreq" id="tranFreq" required>
+                                    <option>One-time</option>
+                                    <option>Daily</option>
+                                    <option>Weekly</option>
+                                    <option>Monthly</option>
+                                    <option>Yearly</option>
+                                </Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for='tranDate'>Date of transaction</Label>
+                                <Input type='date' id='tranDate' required/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="tranAmnt">Amount</Label>
+                                <Input id="tranAmnt" type="number" min="1" step=".01" placeholder='99.99' required/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for='isIncome'>Is this income?&nbsp;&nbsp;&nbsp;&nbsp;</Label>
+                                <Input type='checkbox' id='isIncome'/>
+                            </FormGroup>
+                            <div className="flex-center width-full">
+                                <Button className='primary-button'>Add transaction</Button>
+                            </div>
+                        </Form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
